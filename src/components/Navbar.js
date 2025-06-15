a7106291dd35470becd4c3c764f5245b398f2c48
Deepak <deepakchitravel@gmail.com>
2025-06-15 21:53:01 +0530

    Change made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../css/Navbar.css'; // Custom styling
 
-const languages = [
-  { name: 'HTML', path: '/html' },
-  { name: 'CSS', path: '/css' },
-  { name: 'JavaScript', path: '/javascript' },
-  { name: 'C', path: '/c' },
-  { name: 'Fundamentals', path: '/Fundamentals' },
-  { name: 'MS Office', path: '/msword' },
-  { name: 'Course', path: '/course' },
-];
-
+// ✅ Chapter arrays should come BEFORE the function
 const htmlChapters = [
   { name: 'HTML Introduction', path: '/html/HTMLIntroduction' },
   { name: 'HTML Tags', path: '/html/TagExample' },
@@ -36,90 +27,6 @@ const htmlChapters = [
   { name: 'Link Tag', path: '/html/LinkTag' },
   { name: 'Favicon Tag', path: '/html/FaviconTag' },
 ];
-
-const cChapters = [
-  // Intro & Basics
-  { name: 'C Introduction', path: '/c/CIntro' },
-  { name: 'C Features', path: '/c/CFeatures' },
-  { name: 'C Basics', path: '/c/CBasics' },
-  { name: 'C Comments', path: '/c/CComments' },
-  { name: 'C Shortcut Keys', path: '/c/CShortcutKeys' },
-
-  // Variables & Data Types
-  { name: 'C Identifiers', path: '/c/CIdentifiers' },
-  { name: 'C Variables', path: '/c/CVariables' },
-  { name: 'C Variable Types', path: '/c/VariableTypes' },
-  { name: 'C Datatypes', path: '/c/Datatypes' },
-  { name: 'C Char', path: '/c/Char' },
-  { name: 'C Double', path: '/c/Double' },
-  { name: 'C Int, Float', path: '/c/CSampleIntFloat' },
-
-  // Constants & Storage
-  { name: 'C Const', path: '/c/Const' },
-  { name: 'C Auto Variable', path: '/c/AutoVariable' },
-  { name: 'C Extern', path: '/c/Extern' },
-  { name: 'C Extern Variable', path: '/c/ExternVariable' },
-
-  // Operators
-  { name: 'C Operators', path: '/c/COperators' },
-  { name: 'C Assignment', path: '/c/Assignment' },
-  { name: 'C Increment & Decrement', path: '/c/IncrementDecrement' },
-  { name: 'C Logical', path: '/c/Logical' },
-  { name: 'C Ternary', path: '/c/Ternary' },
-  { name: 'C Misc Operators', path: '/c/MiscOperators' },
-  { name: 'C Bitwise', path: '/c/Bitwise' },
-  { name: 'C Left Shift', path: '/c/LeftShift' },
-  { name: 'C Right Shift', path: '/c/RightShift' },
-  { name: 'C Unary', path: '/c/Unary' },
-
-  // Keywords & Tokens
-  { name: 'C Keywords', path: '/c/CKeywords' },
-  { name: 'C ASCII', path: '/c/ASCII' },
-
-  // Control Statements
-  { name: 'C Control Statement', path: '/c/ControlStatementIntro' },
-  { name: 'C If Statement', path: '/c/IfStatement' },
-  { name: 'C If Else Statement', path: '/c/IfElseStatement' },
-  { name: 'C If Else If Ladder', path: '/c/IfElseIfLadder' },
-  { name: 'C Nested If', path: '/c/NestedIf' },
-  { name: 'C Else', path: '/c/Else' },
-  { name: 'C Switch', path: '/c/SwitchC' },
-  { name: 'C Break', path: '/c/Break' },
-  { name: 'C Continue', path: '/c/Continue' },
-  { name: 'C Goto Statement', path: '/c/GotoStatement' },
-
-  // Loops
-  { name: 'C Loop Intro', path: '/c/LoopIntro' },
-  { name: 'C Loop Types', path: '/c/LoopTypes' },
-  { name: 'C Do While', path: '/c/Do' },
-
-  // Arrays
-  { name: 'C Array Intro', path: '/c/ArrayIntro' },
-  { name: 'C One D Array', path: '/c/OneDArray' },
-  { name: 'C Two D Array', path: '/c/TwoDArray' },
-  { name: 'C Multi D Array', path: '/c/MultiDArray' },
-
-  // Pointers
-  { name: 'C Pointer Intro', path: '/c/PointerIntro' },
-  { name: 'C Pointer Demo', path: '/c/PointerDemo' },
-
-  // Structures & Enums
-  { name: 'C Structure', path: '/c/StructureC' },
-  { name: 'C Enum', path: '/c/Enum' },
-  { name: 'C Enum Datatype', path: '/c/EnumDataType' },
-
-  // Programs (Practice)
-  { name: 'C Add and Mul', path: '/c/AddAndMul' },
-  { name: 'C Sub and Div', path: '/c/SubAndDiv' },
-  { name: 'C EB-Bill', path: '/c/EBBill' },
-  { name: 'C Swap', path: '/c/SwapC' },
-
-  // Misc
-  { name: 'C Format Specifiers', path: '/c/CFormatSpecifiers' },
-  { name: 'C Boolean', path: '/c/Boolean' },
-  { name: 'C Default', path: '/c/Default' },
-];
-
 
 const cssChapters = [
   { name: 'CSS Introduction', path: '/css/CSSIntro' },
@@ -156,6 +63,66 @@ const cssChapters = [
   { name: 'CSS Linear Gradients', path: '/css/CSSLinearGradients' },
 ];
 
+const cChapters = [
+  { name: 'C Introduction', path: '/c/CIntro' },
+  { name: 'C Features', path: '/c/CFeatures' },
+  { name: 'C Basics', path: '/c/CBasics' },
+  { name: 'C Comments', path: '/c/CComments' },
+  { name: 'C Shortcut Keys', path: '/c/CShortcutKeys' },
+  { name: 'C Identifiers', path: '/c/CIdentifiers' },
+  { name: 'C Variables', path: '/c/CVariables' },
+  { name: 'C Variable Types', path: '/c/VariableTypes' },
+  { name: 'C Datatypes', path: '/c/Datatypes' },
+  { name: 'C Char', path: '/c/Char' },
+  { name: 'C Double', path: '/c/Double' },
+  { name: 'C Int, Float', path: '/c/CSampleIntFloat' },
+  { name: 'C Const', path: '/c/Const' },
+  { name: 'C Auto Variable', path: '/c/AutoVariable' },
+  { name: 'C Extern', path: '/c/Extern' },
+  { name: 'C Extern Variable', path: '/c/ExternVariable' },
+  { name: 'C Operators', path: '/c/COperators' },
+  { name: 'C Assignment', path: '/c/Assignment' },
+  { name: 'C Increment & Decrement', path: '/c/IncrementDecrement' },
+  { name: 'C Logical', path: '/c/Logical' },
+  { name: 'C Ternary', path: '/c/Ternary' },
+  { name: 'C Misc Operators', path: '/c/MiscOperators' },
+  { name: 'C Bitwise', path: '/c/Bitwise' },
+  { name: 'C Left Shift', path: '/c/LeftShift' },
+  { name: 'C Right Shift', path: '/c/RightShift' },
+  { name: 'C Unary', path: '/c/Unary' },
+  { name: 'C Keywords', path: '/c/CKeywords' },
+  { name: 'C ASCII', path: '/c/ASCII' },
+  { name: 'C Control Statement', path: '/c/ControlStatementIntro' },
+  { name: 'C If Statement', path: '/c/IfStatement' },
+  { name: 'C If Else Statement', path: '/c/IfElseStatement' },
+  { name: 'C If Else If Ladder', path: '/c/IfElseIfLadder' },
+  { name: 'C Nested If', path: '/c/NestedIf' },
+  { name: 'C Else', path: '/c/Else' },
+  { name: 'C Switch', path: '/c/SwitchC' },
+  { name: 'C Break', path: '/c/Break' },
+  { name: 'C Continue', path: '/c/Continue' },
+  { name: 'C Goto Statement', path: '/c/GotoStatement' },
+  { name: 'C Loop Intro', path: '/c/LoopIntro' },
+  { name: 'C Loop Types', path: '/c/LoopTypes' },
+  { name: 'C Do While', path: '/c/Do' },
+  { name: 'C Array Intro', path: '/c/ArrayIntro' },
+  { name: 'C One D Array', path: '/c/OneDArray' },
+  { name: 'C Two D Array', path: '/c/TwoDArray' },
+  { name: 'C Multi D Array', path: '/c/MultiDArray' },
+  { name: 'C Pointer Intro', path: '/c/PointerIntro' },
+  { name: 'C Pointer Demo', path: '/c/PointerDemo' },
+  { name: 'C Structure', path: '/c/StructureC' },
+  { name: 'C Enum', path: '/c/Enum' },
+  { name: 'C Enum Datatype', path: '/c/EnumDataType' },
+  { name: 'C Add and Mul', path: '/c/AddAndMul' },
+  { name: 'C Sub and Div', path: '/c/SubAndDiv' },
+  { name: 'C EB-Bill', path: '/c/EBBill' },
+  { name: 'C Swap', path: '/c/SwapC' },
+  { name: 'C Format Specifiers', path: '/c/CFormatSpecifiers' },
+  { name: 'C Boolean', path: '/c/Boolean' },
+  { name: 'C Default', path: '/c/Default' },
+];
+
 const msofficeChapters = [
   { name: 'MS Word Introduction', path: '/msword/mswordintro' },
   { name: 'Shortcut Key', path: '/msword/Shortcutkey' },
@@ -182,6 +149,29 @@ const fundamentalsChapters = [
   { name: 'Bit and Byte Conversion', path: '/fundamentals/BitAndComputer' },
 ];
 
+// ✅ Correct placement of getTitleFromPath
+const getTitleFromPath = (pathname) => {
+  const allChapters = [
+    ...htmlChapters,
+    ...cssChapters,
+    ...cChapters,
+    ...msofficeChapters,
+    ...fundamentalsChapters,
+  ];
+  const matched = allChapters.find((chap) => pathname === chap.path);
+  return matched ? matched.name : 'My Web Learning App';
+};
+
+const languages = [
+  { name: 'HTML', path: '/html' },
+  { name: 'CSS', path: '/css' },
+  { name: 'JavaScript', path: '/javascript' },
+  { name: 'C', path: '/c' },
+  { name: 'Fundamentals', path: '/Fundamentals' },
+  { name: 'MS Office', path: '/msword' },
+  { name: 'Course', path: '/course' },
+];
+
 const Navbar = () => {
   const location = useLocation();
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -194,39 +184,54 @@ const Navbar = () => {
     }
   }, [location.pathname]);
 
+  useEffect(() => {
+    const title = getTitleFromPath(location.pathname);
+    document.title = `${title} | My Web Learning App`;
+  }, [location.pathname]);
+
   const handleLangClick = (lang) => {
     setSelectedLang(lang.name);
     setSidebarOpen(true);
   };
 
   const isActiveLang = (lang) => selectedLang === lang.name;
-const renderChapters = () => {
-  const chapters = selectedLang === 'HTML' ? htmlChapters :
-                   selectedLang === 'CSS' ? cssChapters :
-                  selectedLang === 'Fundamentals' ? fundamentalsChapters:
-                  selectedLang === 'MS Office' ? msofficeChapters:
-                   selectedLang === 'C' ? cChapters : [];
-  return (
-    <ul style={{ paddingLeft: '1rem' }}>
-      {chapters.map((chapter) => (
-        <li key={chapter.name}>
-          <Link
-            to={chapter.path}
-            className={location.pathname === chapter.path ? 'active' : ''}
-            onClick={() => setSidebarOpen(false)}
-          >
-            {chapter.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-};
 
+  const renderChapters = () => {
+    const chapters =
+      selectedLang === 'HTML'
+        ? htmlChapters
+        : selectedLang === 'CSS'
+        ? cssChapters
+        : selectedLang === 'Fundamentals'
+        ? fundamentalsChapters
+        : selectedLang === 'MS Office'
+        ? msofficeChapters
+        : selectedLang === 'C'
+        ? cChapters
+        : [];
+    return (
+      <ul style={{ paddingLeft: '1rem' }}>
+        {chapters.map((chapter) => (
+          <li key={chapter.name}>
+            <Link
+              to={chapter.path}
+              className={location.pathname === chapter.path ? 'active' : ''}
+              onClick={() => setSidebarOpen(false)}
+            >
+              {chapter.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    );
+  };
 
   return (
     <nav className="w3-navbar-wrapper">
-      <button className="menu-icon" onClick={() => setSidebarOpen(!sidebarOpen)}>☰</button>
+      <button className="menu-icon" onClick={() => setSidebarOpen(!sidebarOpen)}>
+        ☰
+      </button>
+
       <div className="w3-navbar-scroll">
         {languages.map((lang) => (
           <Link
@@ -239,8 +244,26 @@ const renderChapters = () => {
           </Link>
         ))}
       </div>
+
+      <div className="w3-auth-buttons">
+        <Link to="/signin" className="auth-btn">
+          Sign In
+        </Link>
+        <Link to="/signup" className="auth-btn signup">
+          Sign Up
+        </Link>
+      </div>
+
       <div className={`w3-sidebar ${sidebarOpen ? 'open' : ''}`}>
-        <button className="close-btn" onClick={() => { setSidebarOpen(false); setSelectedLang(null); }}>×</button>
+        <button
+          className="close-btn"
+          onClick={() => {
+            setSidebarOpen(false);
+            setSelectedLang(null);
+          }}
+        >
+          ×
+        </button>
         {renderChapters()}
       </div>
     </nav>
--- src/pages/html/HTMLLayout.js
+++ src/pages/html/HTMLLayout.js
@@ -1,39 +1,66 @@
 // src/pages/fundamentals/HTMLLayout.js
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import HTMLSidebar from '../../components/HTMLSidebar';
 import '../../css/FundamentalsLayout.css';
 
 const HTMLLayout = () => {
   return (
-    <div className="layout-container">
-      {/* Left Sidebar */}
-      <HTMLSidebar />
+    <div className="layout-container" style={{ display: 'flex' }}>
+      
+      {/* Left Advertisement */}
+      <div className="ad-section" style={{ width: '15%', padding: '10px', background: '#f9f9f9' }}>
+        <div className="ad-box">
+          <p><strong>🔥 Smartwatch Deal</strong></p>
+          <a
+            href="https://www.amazon.in/dp/B09Y1FJ7H8"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: 'none', color: 'inherit' }}
+          >
+            <img
+              src="https://m.media-amazon.com/images/I/71qU9mvL0kL._SX679_.jpg"
+              alt="Smartwatch"
+              style={{ width: '100%', borderRadius: '8px' }}
+            />
+            <p style={{ marginTop: '10px' }}>boAt Smartwatch - ₹1,499</p>
+            <ul style={{ fontSize: '0.9rem', paddingLeft: '16px' }}>
+              <li>1.52″ AMOLED Display</li>
+              <li>Bluetooth Calling</li>
+              <li>Multiple Watch Faces</li>
+            </ul>
+          </a>
+        </div>
+      </div>
 
       {/* Main Content */}
-      <div className="main-content">
+      <div className="main-content" style={{ flex: 1, padding: '20px' }}>
         <Outlet />
       </div>
 
       {/* Right Advertisement */}
- <div className="ad-section">
-  <div className="ad-box">
-    <p><strong>🔥 Naruto Special</strong></p>
-    <a
-      href="https://en.wikipedia.org/wiki/Naruto"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ textDecoration: 'none', color: 'inherit' }}
-    >
-      <img
-        src="https://upload.wikimedia.org/wikipedia/en/9/94/NarutoCoverTankobon1.jpg"
-        alt="Naruto Cover"
-        style={{ width: '100%', borderRadius: '8px' }}
-      />
-      <p style={{ marginTop: '10px' }}>Watch Naruto – Legendary Ninja Journey!</p>
-    </a>
-  </div>
-</div>
+      <div className="ad-section" style={{ width: '15%', padding: '10px', background: '#f9f9f9' }}>
+        <div className="ad-box">
+          <p><strong>🎧 Headphones Offer</strong></p>
+          <a
+            href="https://www.amazon.in/dp/B0863TXGM3"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: 'none', color: 'inherit' }}
+          >
+            <img
+              src="https://m.media-amazon.com/images/I/61D4Z3yKPAL._SL1500_.jpg"
+              alt="Sony Headphones"
+              style={{ width: '100%', borderRadius: '8px' }}
+            />
+            <p style={{ marginTop: '10px' }}>Sony WH-CH520 - ₹3,989</p>
+            <ul style={{ fontSize: '0.9rem', paddingLeft: '16px' }}>
+              <li>35 Hrs Battery</li>
+              <li>Bluetooth 5.0</li>
+              <li>On-Ear Comfort</li>
+            </ul>
+          </a>
+        </div>
+      </div>
 
     </div>
   );
